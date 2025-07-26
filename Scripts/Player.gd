a5720a6b64d182029a5712883f176829b4b65b64
mathew <mathew.tran.dev@gmail.com>
2025-07-26 08:08:36 +0800

    start project, pull some files from two dimension project to repurpose for this project

---
+++ Scripts/Player.gd
@@ -0,0 +1,132 @@
+extends RigidBody2D
+
+class_name Player
+
+var Eyes = []
+
+var MoveSpeed = 600
+var MaxLength = 600
+@onready var TongueEndClass = preload("res://Prefab/TongueEnd.tscn")
+var TongueEndRef = null
+
+var bCanMove = true
+var bIsDead = false
+
+
+func _ready():
+	pass
+
+func FreezeBody():
+	freeze = true
+	for child in get_children():
+		if child is RigidBody2D:
+			child.freeze = true
+			
+	if HasTongue():
+		TongueEndRef.bEnabled = false
+
+	
+func UpdateEyes(delta):
+	for eye in Eyes:
+		if eye:
+			eye.look_at(get_global_mouse_position())	
+	
+func _process(delta):
+	if CanMove() == false:
+		return
+		
+	UpdateEyes(delta)		
+		
+	if Input.is_action_just_released("Click"):
+		RevertTongue()
+		
+	if Input.is_action_pressed("Click") and CanUseTongue() and HasTongue() == false:
+		$TongueCooldown.start()
+		RevertTongue()
+		
+		var direction = to_local(get_global_mouse_position()).normalized()
+		$RayCast2D.target_position = direction * MaxLength
+		$RayCast2D.force_raycast_update()
+		
+		TongueEndRef = TongueEndClass.instantiate()
+		TongueEndRef.OwnerObject = self
+		TongueEndRef.MaxLength = MaxLength
+		get_parent().add_child(TongueEndRef)
+		
+		if $RayCast2D.is_colliding():			
+			TongueEndRef.global_position = $RayCast2D.get_collision_point()
+			TongueEndRef.get_node("PinJoint2D").node_b = get_path()
+			TongueEndRef.SetupInitialTracking($RayCast2D.get_collision_point(), $RayCast2D.get_collider().get_path())
+			angular_velocity = 0
+			$RayCast2D.enabled = false
+			TongueEndRef.EmitParticle()
+		else:
+			TongueEndRef.global_position = to_global($RayCast2D.target_position)
+	
+func _physics_process(delta):
+	if CanMove():
+		Move(delta)
+		
+func IsConnected():
+	return 
+		
+func Move(delta):
+	var multiplier = 1
+	if HasTongue():
+		multiplier *= 2
+	if Input.is_action_pressed("MoveLeft"):
+		apply_impulse(Vector2.LEFT * MoveSpeed * delta * multiplier)
+	if Input.is_action_pressed("MoveRight"):
+		apply_impulse(Vector2.RIGHT * MoveSpeed * delta * multiplier)
+
+func CanUseTongue():
+	return $TongueCooldown.time_left == 0.0
+	
+func HasTongue():
+	return is_instance_valid(TongueEndRef)
+	
+	
+func CanMove():
+	return bCanMove and bIsDead == false
+	
+func _input(event):
+	if CanMove() == false:
+		return
+		
+	if event.is_action_pressed("Detach") and CanUseTongue():
+		RevertTongue()
+		
+
+		
+func RevertTongue():
+	$RayCast2D.target_position = Vector2.ZERO
+
+	$RayCast2D.force_raycast_update()
+	if is_instance_valid(TongueEndRef):
+		TongueEndRef.Kill()
+
+
+func _on_tongue_cooldown_timeout():
+	if IsConnected() == false:
+		RevertTongue()
+		$RayCast2D.enabled = true
+
+func Die():
+	if bIsDead:
+		return
+		
+	bIsDead = true
+	
+	FreezeBody()
+	if HasTongue():
+		TongueEndRef.queue_free()
+		
+		
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(2,2), .2)
+	await tween.finished
+	modulate = Color(0,0,0,0)
+	await get_tree().create_timer(.3).timeout	
+
+	print("Game Over")
+	
---
+++ Scripts/TongueEnd.gd
@@ -0,0 +1,104 @@
+extends StaticBody2D
+
+var OwnerObject = null
+
+var bIsKilled = false
+
+var Progress = 0.0
+
+var PullSpeed = 1000
+
+var bInitialImpulse = false
+var MaxLength = 200
+
+var bEnabled = true
+var CollisionPosition = Vector2.ZERO
+var LocalOffset = Vector2.ZERO
+var LocalRotationOffset : float
+
+func SetupInitialTracking(trackedPosition, connectionJointPath):
+	$ConnectionJoint.node_b = connectionJointPath
+	CollisionPosition = trackedPosition
+	LocalOffset = GetSurface().to_local(CollisionPosition)
+	LocalRotationOffset = global_rotation - GetSurface().global_rotation
+	
+func UpdateTrackingPosition():
+	var scaledOffset = LocalOffset
+	var rotatedOffset = scaledOffset.rotated(GetSurface().global_rotation)
+	global_position = GetSurface().global_position + rotatedOffset
+	global_rotation = GetSurface().global_rotation + LocalRotationOffset
+
+func GetSurface():
+	return get_node($ConnectionJoint.node_b)
+	
+func _process(delta):
+	if bEnabled == false:
+		return
+	$Line2D.points[1] = to_local(OwnerObject.global_position)
+	if bIsKilled:
+		Progress += delta * 40
+		$Line2D.points[0] = to_local(lerp(self.global_position, OwnerObject.global_position, Progress))
+		$Sprite2D.visible = false
+		if Progress >= 1:
+			queue_free()
+	else:
+		$Line2D.points[0] = to_local(self.global_position)
+		if $PinJoint2D.node_b != OwnerObject.get_path() or get_node_or_null($ConnectionJoint.node_b) == null:
+			if $KillTimer.time_left == 0.0:
+				$KillTimer.start()
+				$Line2D.width *= .5
+		else:
+			$Line2D.default_color = Color.RED
+			$Sprite2D.modulate = Color.RED
+			UpdateTrackingPosition()
+	
+func EmitParticle():
+	$CPUParticles2D.emitting = true
+	$AudioStreamPlayer2D.play()
+			
+func _physics_process(delta):
+	if bEnabled == false:
+		return
+	if $PinJoint2D.node_b != OwnerObject.get_path():
+		return
+		
+	var speed = PullSpeed
+	if bInitialImpulse:
+		speed *= 10
+		
+	if Input.is_action_pressed("Down"):
+		IncreaseTongueLength(speed * delta)
+	elif Input.is_action_pressed("Up"):
+		DecreaseTongueLength(speed * delta)
+
+	else:
+		bInitialImpulse = true
+	
+	
+func IncreaseTongueLength(amount):
+	if global_position.distance_to(OwnerObject.global_position) >= MaxLength:
+		return
+	$PinJoint2D.node_b = get_path()
+	var direction = (OwnerObject.global_position - global_position).normalized()
+	OwnerObject.global_position += direction * 20
+	bInitialImpulse = false
+	$PinJoint2D.node_b = OwnerObject.get_path()
+	
+func DecreaseTongueLength(amount):
+	if global_position.distance_to(OwnerObject.global_position) <= 100:
+		return
+	$PinJoint2D.node_b = get_path()
+	var direction = (OwnerObject.global_position - global_position).normalized()
+	OwnerObject.global_position += direction * -20
+	bInitialImpulse = false	
+	$PinJoint2D.node_b = OwnerObject.get_path()
+	
+func _on_kill_timer_timeout():
+	Kill()
+
+func Kill():
+	if bIsKilled:
+		return
+	bIsKilled = true
+
+			
